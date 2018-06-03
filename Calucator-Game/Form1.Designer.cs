89aca00c49018143769c206087ccbb6ad250ec3b
Montaro2017 <kertlk@qq.com>
2018-06-03 19:00:30 +0800

    I don't know wwhat changed

--- Calucator-Game/Form1.Designer.cs
+++ Calucator-Game/Form1.Designer.cs
@@ -109,7 +109,6 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
